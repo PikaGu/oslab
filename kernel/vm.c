5ef5ae05d20ce85eda59f0723b12af136b42195a
PikaGu <icpcpikagu@gmail.com>
2023-04-20 15:16:23 +0800

    vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -433,6 +433,24 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   }
 }
 
-void vmprint(pagetable_t pagetable) {
+void pteprint(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      printf("..");
+      for (int j = 0; j < level; j++) {
+        printf(" ..");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) { 
+        pteprint((pagetable_t) child, level+1);
+      }
+    }
+  }
+}
 
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  pteprint(pagetable, 0);
 }
